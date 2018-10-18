b1ec0ef352847e24a5b6ea77eb482503b09e07dd
Tom Quinonero <tom@tomtomtom.fr>
2018-10-18 15:21:52 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Demo for the app</h1>
+    This repository is an example for a medium post on how to create a 
+    vue component for the community.<br/>
+    This post will walk you through the process of creating a npm ready
+    js package, and make a demo website using Github Pages.
+    <a href="">Have a look at the post here</a>
+    <h2>The component itself</h2>
+    <my-component/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyComponent
   }
 }
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content">
+    <h2>It works !</h2>
+    <p>I'm the component that needs to be packaged</p>
+  </div>
+</template>
+<style scoped lang="scss">
+.content{
+  border-bottom: 5px solid #341212;
+}
+</style>
