56b9a33449f3274dba8a1ffc6f5d94adb8f18141
Tom Quinonero <tom@tomtomtom.fr>
2018-10-18 15:45:05 +0200

    add story link

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     vue component for the community.<br/>
     This post will walk you through the process of creating a npm ready
     js package, and make a demo website using Github Pages.
-    <a href="">Have a look at the post here</a>
+    <a href="https://medium.com/@tomquinonero/creating-a-vuejs-component-for-the-community-a-walk-through-485ea88922c7">Have a look at the post here</a>
     <h2>The component itself</h2>
     <my-component/>
   </div>
